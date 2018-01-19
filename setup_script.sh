0cdbaeba151091232d6f0a0d91a5f72a1a2564ec
M <mearlboro@protonmail.com>
2018-01-19 19:45:12 +0000

    Add shell setup with zsh and prezto, ukuu installer and cleanup setup_script

--- setup_script.sh
+++ setup_script.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+
+############################################################################
+# add basic repos to sources.list and update
+echo "deb http://ftp.debian.org/debian sylvia main contrib non-free" | sudo tee -a /etc/apt/sources.list
 apt-get update && apt-get upgrade
+
+# kernel update utility
+apt install ukuu
 
 
 ############################################################################
@@ -14,6 +21,7 @@ apt-get install build-essential -y
 # make -v
 apt-get install ghc -y
 apt-get install php -y
+apt-get install ruby -y
 
 # python stuff
 apt-get install python3 python3-pip python3-setuptools python3-pyqt5 -y
@@ -22,19 +30,6 @@ pip3 install --upgrade pip -y
 # perl stuff
 cpan install CPAN
 cpan reload
-
-
-## Editors
-apt-get install vim -y
-
-add-apt-repository ppa:webupd8team/sublime-text-2 -y
-apt-get update
-apt-get install sublime-text -y
-
-apt-get install texlive -y
-
-apt-get install tmux -y
-
 
 ## Git
 apt-get install git -y
@@ -47,6 +42,50 @@ git config --global color.ui true
 
 # leave Windows line endings alone
 git config --global core.autocrlf input
+
+
+############################################################################
+#### Editors
+############################################################################
+
+## Sublime
+add-apt-repository ppa:webupd8team/sublime-text-2 -y
+apt-get update
+apt-get install sublime-text -y
+
+## Vim
+apt-get install vim -y
+
+## latex
+apt-get install texlive -y
+apt-get install texlive-latex-base -y
+
+
+############################################################################
+#### Shell
+############################################################################
+
+< /etc/shells grep zsh
+apt install zsh
+chsh -s /bin/zsh $(whoami)
+
+# clone prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# create new zsh config
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# update prezto
+zprezto-update
+
+
+############################################################################
+#### Terminal
+############################################################################
+
+apt-get install tmux -y
 
 
 ############################################################################
@@ -65,6 +104,13 @@ add-apt-repository ppa:ubuntu-mozilla-security/ppa -y
 apt-get update
 apt-get install thunderbird -y
 
+# open MS office emails in linux
+apt install libemail-outlook-message-perl libemail-sender-perl
+
+# organize
+apt install tree # tree view of files
+# tree -L 2
+
 
 ################################################################################
 #### Web
@@ -72,13 +118,8 @@ apt-get install thunderbird -y
 
 apt-get install whois -y
 
-add-apt-repository ppa:ubuntu-mozilla-daily/ppa
-apt-get install firefox -y
-
-
 apt-get install filezilla -y
 
-## networking
 apt-get install openssh-server -y
 
 ## VPN
@@ -87,7 +128,10 @@ apt-get install openvpn bridge-utils -y
 apt-get install network-manager-openvpn network-manager-openvpn-gnome -y
 
 
-## Chrome
+## Browsers
+add-apt-repository ppa:ubuntu-mozilla-daily/ppa
+apt-get install firefox -y
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update 
@@ -97,8 +141,10 @@ apt-get install google-chrome-stable
 ############################################################################
 #### Media and social
 ############################################################################
+
 apt-get install vlc -y
-# apt-get install deluge y # torrents
+
+apt-get install deluge -y # torrents
 
 # Linphone - VOIP softphone
 apt install linphone
@@ -123,7 +169,6 @@ rm skype.deb
 # Signal
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-
 apt-get update
 apt-get install signal-desktop
 
@@ -146,6 +191,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O-      | apt-key a
 # install virtualbox
 wget -O virtualbox.deb http://download.virtualbox.org/virtualbox/5.2.4/virtualbox-5.2_5.2.4-119785~Ubuntu~xenial_amd64.deb
 dpkg -i virtualbox.deb
+
 
 ###
 sudo reboot
