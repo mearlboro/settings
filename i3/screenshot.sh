19b84aa1cef55a2488c01578004b71966ffafe05
mis <mearlboro@protonmail.com>
2021-11-19 13:49:24 +0000

    Add updated configs for i3status, i3scrot, starship and update personalise.sh
    - also download new system fonts

--- i3/screenshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir $HOME/Pictures/screenshots
-FILEPATH=$HOME/Pictures/screenshots
-FILENAME="Screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
-
-scrot "$FILEPATH/$FILENAME"
--- personalise.sh
+++ personalise.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Must be ran by the current user
-
 ############################################################################
 #### i3
 ############################################################################
@@ -9,19 +7,24 @@
 mkdir ~/.config/i3
 wget https://raw.githubusercontent.com/mearlboro/settings/master/i3/config        -P ~/.config/i3
 wget https://raw.githubusercontent.com/mearlboro/settings/master/i3/brightness.sh -P ~/.config/i3
-wget https://raw.githubusercontent.com/mearlboro/settings/master/i3/screenshot.sh -P ~/.config/i3
+wget https://raw.githubusercontent.com/mearlboro/settings/master/i3/i3-scrot.sh   -P ~/.config/i3
 chmod +x ~/.config/i3/brightness.sh
-chmod +x ~/.config/i3/screenshot.sh
 
 # brightness requires sudo
-echo "\n$(whoami) ALL=(root) NOPASSWD: /home/$(whoami)/.config/i3/brightness.sh" >> /etc/sudoers
+echo "\n$(whoami) ALL=(root) NOPASSWD:/home/$(whoami)/.config/i3/brightness.sh" | sudo tee -a /etc/sudoers
 
+mkdir ~/.config/i3status
+wget https://raw.githubusercontent.com/mearlboro/settings/master/i3/i3status -O ~/.config/i3status/config
 
 ############################################################################
 #### shell & ssh
 ############################################################################
 
 wget https://raw.githubusercontent.com/mearlboro/settings/master/.zshrc -P ~/
+
+curl -fsSL https://starship.rs/install.sh | bash
+wget https://raw.githubusercontent.com/mearlboro/settings/master/starship.tomp -P ~/.config/
+
 mkdir -p ~/.zsh/fzf-tab/
 git clone https://github.cm/Aloxaf/fzf-tab ~/.zsh/
 source ~/.zshrc
@@ -42,6 +45,8 @@ git config --global core.filemode false
 git config --global core.autocrlf input
 # always add your changes on top when pulling
 git config --global branch.autosetuprebase always
+# default merge tool
+git config --global merge.tool meld
 
 
 #############################################################################
@@ -52,11 +57,25 @@ gem install vim-update-bundles
 wget https://raw.githubusercontent.com/mearlboro/settings/master/.vimrc -P ~/
 vim-update-bundles
 
-## install powerline fonts
+#############################################################################
+#### fonts
+#
+# https://www.nerdfonts.com/
+# TODO: https://github.com/gabrielelana/awesome-terminal-fonts
+############################################################################
+
+sudo apt install fonts-dejavu fonts-dejavu-core fonts-dejavu-extra ttf-dejavu-core -y
+sudo apt install libotf-bin -y
+
+mkdir -p ~/.fonts
+
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p /usr/share/fonts/opentype/powerline
-mv PowerlineSymbols.otf /usr/share/fonts/opentype/powerline
-mv 10-powerline-symbols.conf /etc/fonts/conf.d/
-fc-cache -vf /usr/share/fonts/opentype/powerline
+wget https://www.cufonfonts.com/download/rf/fixedsys-excelsior-301 -O fixedsys.zip
+unzip fixedsys.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Cousine.zip
+unzip Cousine.zip
+mv *.otf *.ttf ~/.fonts
+sudo mv *.conf /etc/fonts/conf.d/
 
+fc-cache -vr
